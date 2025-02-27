cab922cf8a910bf4a3a8fb43a5d4cc04a75bb3f4
publicclassCarlangas <carlos_aalzate@soy.sena.edu.co>
2025-02-27 08:44:29 -0500

    Resolved merge conflict in server.js by keeping both routes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
+
+import medicinaRoutes from "./routes/medicinaRoutes.js"; // Importing medicina routes
+import categoryRoutes from "./routes/categoryRoutes.js"; // Importing category routes
 
 dotenv.config();
 connectDB();
@@ -11,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/categories", categoryRoutes);
+
+app.use("/api/medicinas", medicinaRoutes); // Adding medicina routes
+app.use("/api/categories", categoryRoutes); // Adding category routes
 
 export default app;
